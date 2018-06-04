a0e342e1b11a167c27b6dcd714d6b329a559ff8a
Zach Bayoff <zrbayoff@gmail.com>
2018-06-04 07:31:43 -0400

    working on photo gallery

--- angular-src/src/app/components/photo-gallery/photo-gallery.component.ts
+++ angular-src/src/app/components/photo-gallery/photo-gallery.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PhotosService } from '../../service/photos.service';
+
 @Component({
   selector: 'app-photo-gallery',
   templateUrl: './photo-gallery.component.html',
@@ -7,12 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoGalleryComponent implements OnInit {
 
-
+  selectedImage: string;
   modalOpen: boolean;
+  photos: any;
 
-  constructor() {
+  constructor(private photosService: PhotosService) {
     this.modalOpen = false;
   }
+
+  searchPhotos(searchTerm) {
+    this.photosService.searchPhotos(searchTerm)
+      .subscribe(
+        data => {
+          console.log(data);
+
+          // if returns a results array
+          this.photos = data.results;
+
+          // set first image as the selected image
+          this.selectImage(this.photos[0].urls.regular);
+
+          console.log(this.photos);
+        },
+        err => console.log(err)
+      );
+  }
+
+  selectImage(imgUrl) {
+    this.selectedImage = imgUrl;
+  }
+
+
+
+
+
 
 
   openModal() {
@@ -20,7 +50,7 @@ export class PhotoGalleryComponent implements OnInit {
     document.body.classList.add('modal-open');
   }
 
-  closeModal () {
+  closeModal() {
     this.modalOpen = false;
   }
 
--- angular-src/src/app/service/photos.service.ts
+++ angular-src/src/app/service/photos.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { Observable, of } from 'rxjs';
+
+import { map, filter, switchMap, catchError } from 'rxjs/operators';
+
+// import { Trip } from '../models/trip';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotosService {
 
-  constructor() { }
+  private endpoint = 'https://api.unsplash.com';
+  private appID = '85cc7fab200fd57d69193fe7e68f784ad92294d19ac08569c26920cf01d44ae0';
+
+  searchPhotos(query) {
+    return this.http.get(`${this.endpoint}/search/photos?client_id=${this.appID}&page=1&query=${query}`)
+      .pipe(
+        map((res) => res.json()),
+      catchError(err => of([]))
+    );
+  }
+
+  constructor(private http: Http) { }
+
+
 }
